a2a5a1986dc7221c31561ddf7c86357651afc92d
Janani161 <134822439+Janani161@users.noreply.github.com>
2023-05-29 23:42:52 +0530

    Update PrintDuplicatesInArray.java

--- assigment2/PrintDuplicatesInArray.java
+++ assigment2/PrintDuplicatesInArray.java
@@ -5,7 +5,7 @@ public class PrintDuplicatesInArray {
 	public static void main(String[] args) {
 		
 		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
-		
+		System.out.println("Length of array is:");
 		int length=arr.length;
 		
 		System.out.println(length);
