4c9ee884abc2a37bcb682eca55460b1cb4f6c469
Janani B <134822439+Janani161@users.noreply.github.com>
2023-07-08 09:04:00 +0530

    Delete assigment directory

--- assigment/Language.java
+++
@@ -1,8 +0,0 @@
-package week3.day1.assigment;
-
-public interface Language {
-	
-	public void Java();
-		
-
-}
--- assigment/TestTool.java
+++
@@ -1,7 +0,0 @@
-package week3.day1.assigment;
-
-public interface TestTool {
-	
-	public void Selenium();
-
-}
